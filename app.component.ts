83e6935a3a08cb120f07d51de583627508964902
Ujjwal Technical Tips <ujjwaltechi1@gmail.com>
2018-10-02 20:37:18 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template:`<h1>Ujjwal Technical Tips</h1>`,
+
+  styles:[`h1{color:red; font-size:74px}`],
+})
+export class AppComponent {
+  FirstName = "Pushpendra";
+  LastName =  "Tripathi";
+}
